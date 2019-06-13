cd30b8d6473a3b1fcbb0911c80b80d78fa2efd5c
wangyds <wangyds@yonyou.com>
2019-06-13 12:55:41 +0800

    create new project

---
+++ src/main/java/com/fanyi/andnow/AndnowApplication.java
@@ -0,0 +1,13 @@
+package com.fanyi.andnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndnowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndnowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fanyi/andnow/AndnowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fanyi.andnow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AndnowApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
