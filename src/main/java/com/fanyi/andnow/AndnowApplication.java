fad09717c75a3ae9300dde35996a2016f0a54e10
wangyds <wangyds@yonyou.com>
2019-06-14 17:20:35 +0800

    update

--- src/main/java/com/fanyi/andnow/AndnowApplication.java
+++ src/main/java/com/fanyi/andnow/AndnowApplication.java
@@ -3,6 +3,12 @@ package com.fanyi.andnow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动入口类
+ *
+ * @author wangyds
+ * @date 2019/06/13
+ */
 @SpringBootApplication
 public class AndnowApplication {
 
