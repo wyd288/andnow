735b8d11e8025915005736b2f436087be83ee7af
wangyds <wangyds@yonyou.com>
2019-06-17 08:32:38 +0800

    update

--- src/main/java/com/fanyi/andnow/common/vo/User.java
+++ src/main/java/com/fanyi/andnow/common/vo/User.java
@@ -1,5 +1,7 @@
 package com.fanyi.andnow.common.vo;
 
+import java.util.Date;
+
 /**
  * 用户实体类
  *
@@ -7,4 +9,26 @@ package com.fanyi.andnow.common.vo;
  * @date 2019/06/13
  */
 public class User {
+
+    private	Integer pk_user;
+    private Integer pk_group;
+    private Integer pk_org;
+    private String code;
+    private String name;
+    private String password;
+    private	Integer sex;
+    private Date birthday;
+    private String phone;
+    private String address;
+    private String note;
+    private Integer creator;
+    private Date creationtime;
+    private Integer modifier;
+    private Date modifiedtime;
+    private Integer dr;
+    private Date ts;
+
+
+
+
 }
