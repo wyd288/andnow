4cda5d32cd8478c2cc6ce5859dc34291fb86c664
wangyds <wangyds@yonyou.com>
2019-06-25 18:53:36 +0800

    实体类使用lombok

--- src/main/java/com/fanyi/andnow/common/vo/basedata/UserVO.java
+++ src/main/java/com/fanyi/andnow/common/vo/basedata/UserVO.java
@@ -1,7 +1,15 @@
 package com.fanyi.andnow.common.vo.basedata;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+/**
+ * 用户实体类
+ *
+ * @author wangyds
+ * @date 2019/6/25
+ */
+@Data
 public class UserVO {
     private Integer pkUser;
 
@@ -51,195 +59,5 @@ public class UserVO {
 
     private Date ts;
 
-    public Integer getPkUser() {
-        return pkUser;
-    }
 
-    public void setPkUser(Integer pkUser) {
-        this.pkUser = pkUser;
-    }
-
-    public Integer getPkGroup() {
-        return pkGroup;
-    }
-
-    public void setPkGroup(Integer pkGroup) {
-        this.pkGroup = pkGroup;
-    }
-
-    public Integer getPkOrg() {
-        return pkOrg;
-    }
-
-    public void setPkOrg(Integer pkOrg) {
-        this.pkOrg = pkOrg;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code == null ? null : code.trim();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
-
-    public Byte getPwdlevel() {
-        return pwdlevel;
-    }
-
-    public void setPwdlevel(Byte pwdlevel) {
-        this.pwdlevel = pwdlevel;
-    }
-
-    public String getPwdparam() {
-        return pwdparam;
-    }
-
-    public void setPwdparam(String pwdparam) {
-        this.pwdparam = pwdparam == null ? null : pwdparam.trim();
-    }
-
-    public Byte getIdentityverifycode() {
-        return identityverifycode;
-    }
-
-    public void setIdentityverifycode(Byte identityverifycode) {
-        this.identityverifycode = identityverifycode;
-    }
-
-    public Date getEnabledate() {
-        return enabledate;
-    }
-
-    public void setEnabledate(Date enabledate) {
-        this.enabledate = enabledate;
-    }
-
-    public Date getDisabledate() {
-        return disabledate;
-    }
-
-    public void setDisabledate(Date disabledate) {
-        this.disabledate = disabledate;
-    }
-
-    public Byte getIslocked() {
-        return islocked;
-    }
-
-    public void setIslocked(Byte islocked) {
-        this.islocked = islocked;
-    }
-
-    public Integer getPkPsndoc() {
-        return pkPsndoc;
-    }
-
-    public void setPkPsndoc(Integer pkPsndoc) {
-        this.pkPsndoc = pkPsndoc;
-    }
-
-    public Integer getDoctype() {
-        return doctype;
-    }
-
-    public void setDoctype(Integer doctype) {
-        this.doctype = doctype;
-    }
-
-    public Integer getUsertype() {
-        return usertype;
-    }
-
-    public void setUsertype(Integer usertype) {
-        this.usertype = usertype;
-    }
-
-    public Date getPkUsergroup() {
-        return pkUsergroup;
-    }
-
-    public void setPkUsergroup(Date pkUsergroup) {
-        this.pkUsergroup = pkUsergroup;
-    }
-
-    public Byte getEnablestate() {
-        return enablestate;
-    }
-
-    public void setEnablestate(Byte enablestate) {
-        this.enablestate = enablestate;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note == null ? null : note.trim();
-    }
-
-    public Integer getCreator() {
-        return creator;
-    }
-
-    public void setCreator(Integer creator) {
-        this.creator = creator;
-    }
-
-    public Date getCreationtime() {
-        return creationtime;
-    }
-
-    public void setCreationtime(Date creationtime) {
-        this.creationtime = creationtime;
-    }
-
-    public Integer getModifier() {
-        return modifier;
-    }
-
-    public void setModifier(Integer modifier) {
-        this.modifier = modifier;
-    }
-
-    public Date getModifiedtime() {
-        return modifiedtime;
-    }
-
-    public void setModifiedtime(Date modifiedtime) {
-        this.modifiedtime = modifiedtime;
-    }
-
-    public Byte getDr() {
-        return dr;
-    }
-
-    public void setDr(Byte dr) {
-        this.dr = dr;
-    }
-
-    public Date getTs() {
-        return ts;
-    }
-
-    public void setTs(Date ts) {
-        this.ts = ts;
-    }
 }
--- src/main/java/com/fanyi/andnow/common/vo/basedata/UsergroupVO.java
+++ src/main/java/com/fanyi/andnow/common/vo/basedata/UsergroupVO.java
@@ -1,7 +1,15 @@
 package com.fanyi.andnow.common.vo.basedata;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+/**
+ * 用户组实体类
+ *
+ * @author wangyds
+ * @date 2019/6/25
+ */
+@Data
 public class UsergroupVO {
     private Integer pkUsergroup;
 
@@ -31,115 +39,5 @@ public class UsergroupVO {
 
     private Date ts;
 
-    public Integer getPkUsergroup() {
-        return pkUsergroup;
-    }
 
-    public void setPkUsergroup(Integer pkUsergroup) {
-        this.pkUsergroup = pkUsergroup;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code == null ? null : code.trim();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public String getInnercode() {
-        return innercode;
-    }
-
-    public void setInnercode(String innercode) {
-        this.innercode = innercode == null ? null : innercode.trim();
-    }
-
-    public Integer getPkSuperior() {
-        return pkSuperior;
-    }
-
-    public void setPkSuperior(Integer pkSuperior) {
-        this.pkSuperior = pkSuperior;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version == null ? null : version.trim();
-    }
-
-    public Byte getEnablestate() {
-        return enablestate;
-    }
-
-    public void setEnablestate(Byte enablestate) {
-        this.enablestate = enablestate;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note == null ? null : note.trim();
-    }
-
-    public Integer getCreator() {
-        return creator;
-    }
-
-    public void setCreator(Integer creator) {
-        this.creator = creator;
-    }
-
-    public Date getCreationtime() {
-        return creationtime;
-    }
-
-    public void setCreationtime(Date creationtime) {
-        this.creationtime = creationtime;
-    }
-
-    public Integer getModifier() {
-        return modifier;
-    }
-
-    public void setModifier(Integer modifier) {
-        this.modifier = modifier;
-    }
-
-    public Date getModifiedtime() {
-        return modifiedtime;
-    }
-
-    public void setModifiedtime(Date modifiedtime) {
-        this.modifiedtime = modifiedtime;
-    }
-
-    public Byte getDr() {
-        return dr;
-    }
-
-    public void setDr(Byte dr) {
-        this.dr = dr;
-    }
-
-    public Date getTs() {
-        return ts;
-    }
-
-    public void setTs(Date ts) {
-        this.ts = ts;
-    }
 }
